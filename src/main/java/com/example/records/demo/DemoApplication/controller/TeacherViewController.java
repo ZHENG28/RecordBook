00743f0672e891ebd2b2e69740be9b56b9afa8e9
Syaro <1215791340@qq.com>
2021-11-16 21:55:02 +0800

    文件结构修改，修改网页Mappping规则

---
+++ src/main/java/com/example/records/demo/DemoApplication/controller/TeacherViewController.java
@@ -0,0 +1,15 @@
+package com.example.records.demo.DemoApplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teacher")
+public class TeacherViewController {
+
+    @GetMapping("/cet46")
+    public String cet(){
+        return "teacher/views/cet46";
+    }
+}
