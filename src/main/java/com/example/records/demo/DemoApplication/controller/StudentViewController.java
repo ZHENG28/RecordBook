7d0aa322eae8d7ad143169e4dca4d30c466a8f29
Syaro <1215791340@qq.com>
2021-11-23 17:50:57 +0800

    学生端，课程表部分部分完成。

---
+++ src/main/java/com/example/records/demo/DemoApplication/controller/StudentViewController.java
@@ -0,0 +1,15 @@
+package com.example.records.demo.DemoApplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentViewController {
+
+    @GetMapping("/Schedule")
+    public String Schedule(){
+        return "student/views/Schedule";
+    }
+}
