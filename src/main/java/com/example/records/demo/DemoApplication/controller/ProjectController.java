68979f056c91d1da383aa4b1a55350bbd4f245ff
86183 <309115015@qq.com>
2021-12-01 22:44:39 +0800

    学生立项前后端走通，学生能提交自身立项情况（后端使用学生ID查询学生时要使用重写的findById(Long id)方法）

--- src/main/java/com/example/records/demo/DemoApplication/controller/ProjectController.java
+++ src/main/java/com/example/records/demo/DemoApplication/controller/ProjectController.java
@@ -1,10 +1,13 @@
 package com.example.records.demo.DemoApplication.controller;
 
 import com.example.records.demo.DemoApplication.entity.Project;
+import com.example.records.demo.DemoApplication.entity.Student;
 import com.example.records.demo.DemoApplication.service.ProjectService;
+import com.example.records.demo.DemoApplication.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,6 +22,9 @@ public class ProjectController
 {
     @Autowired
     ProjectService projectService;
+
+    @Autowired
+    StudentService studentService;
 
     @PostMapping("/queryStuProject")
     @ResponseBody
@@ -37,5 +43,25 @@ public class ProjectController
         }
 
         return response;
+    }
+
+    @PostMapping("/saveStu")
+    @ResponseBody
+    public Object saveStuProject(@RequestBody Map<String, Object> info)
+    {
+        List<Map<String, Object>> projects = (List<Map<String, Object>>) info.get("projects");
+        Student stu = studentService.findStuById(Long.valueOf((Integer) info.get("student")));
+
+        List<Project> projectList = new ArrayList<>();
+        for (Map<String, Object> map : projects) {
+            Project p = new Project();
+            p.setName(String.valueOf(map.get("name")));
+            p.setTime(String.valueOf(map.get("time")));
+            p.setRanking(String.valueOf(map.get("rank")));
+            p.setStudent(stu);
+            projectList.add(p);
+        }
+        projectService.saveStuProject(projectList);
+        return null;
     }
 }
--- src/main/java/com/example/records/demo/DemoApplication/service/StudentService.java
+++ src/main/java/com/example/records/demo/DemoApplication/service/StudentService.java
@@ -1,7 +1,18 @@
 package com.example.records.demo.DemoApplication.service;
 
+import com.example.records.demo.DemoApplication.entity.Student;
+import com.example.records.demo.DemoApplication.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentService {
+public class StudentService
+{
+    @Autowired
+    StudentRepository studentRepository;
+
+    public Student findStuById(Long stuId)
+    {
+        return studentRepository.findById(stuId).orElseThrow();
+    }
 }
