2883dcce1cbdd7e8aa03c65cc99d6b2221dc8be0
MICRACY <1415163591@qq.com>
2022-04-11 15:27:25 +0800

    修改了学生首页的bug；教师查看学生个人简历逻辑完成

--- src/main/java/com/example/records/demo/DemoApplication/controller/StudentController.java
+++ src/main/java/com/example/records/demo/DemoApplication/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.example.records.demo.DemoApplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
-	
+	//学生主页内容加载
+    @GetMapping("/studentIndexContent")
+    public String StudentIndexContent(){
+        return "student/views/studentIndex";
+    }
 }
