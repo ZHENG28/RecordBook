59cc426c3ff8f0fbc8ffd1b024a78a6dd1e2ded8
Syaro <1215791340@qq.com>
2022-09-10 04:00:50 +0800

    新增教师端创建活动页面 更新侧边栏icon

--- src/main/java/com/example/records/demo/DemoApplication/controller/TeacherViewController.java
+++ src/main/java/com/example/records/demo/DemoApplication/controller/TeacherViewController.java
@@ -1,17 +1,12 @@
 package com.example.records.demo.DemoApplication.controller;
 
-import com.example.records.demo.DemoApplication.entity.CClass;
-import com.example.records.demo.DemoApplication.repository.CClassRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 @RequestMapping("/teacher")
@@ -44,5 +39,10 @@ public class TeacherViewController {
     @GetMapping("/views/timetable")
     public String timetable(){
         return "teacher/views/timetable";
+    }
+
+    @GetMapping("/views/createactivity")
+    public String create_activity(){
+        return "teacher/views/create-activity";
     }
 }
